29985e3a99356926bba6c37a15d8bf3b36332433
Emi ONeal <atashiemichandesu@gmail.com>
2023-04-02 02:44:43 -0400

    Added POST and DELETE requests in frontend

--- routes/bountyRouter.js
+++ routes/bountyRouter.js
@@ -3,8 +3,8 @@ const bountyRouter = express()
 const {v4: uuidv4} = require('uuid')
 
 const bounties = [
-    {firstName: "Han", lastName: "Solo", living: true, bountyAmount: 20000, type: "jedi", _id: uuidv4()},
-    {firstName: "Darth", lastName: "Maul", living: false, bountyAmount: 100000, type: "sith", _id: uuidv4()}
+    {firstName: "Han", lastName: "Solo", living: true, bountyAmount: 20000, type: "Jedi", _id: uuidv4()},
+    {firstName: "Darth", lastName: "Maul", living: false, bountyAmount: 100000, type: "Sith", _id: uuidv4()}
 
 ]
 
@@ -18,7 +18,7 @@ bountyRouter.route("/")
         const newBounty = req.body
         newBounty._id = uuidv4()
         bounties.push(newBounty)
-        res.send(`Successfully added ${newBounty.firstName + " " + newBounty.lastName} bounty to the list!`)
+        res.send(newBounty)
     })
 
 
